345f13e1696b447ce2ddf5909f3eabe12687d311
pardod <dhunterpardo@gmail.com>
2024-11-14 00:44:17 -0500

    Initial:

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://deybwfrintyvmyeyabpd.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRleWJ3ZnJpbnR5dm15ZXlhYnBkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEzMzI0ODcsImV4cCI6MjA0NjkwODQ4N30.4C39fiqp729UUn72seN5uNYD3tLpdlXiFimkcrmd644';
-const hCaptchaSiteKey = 'c3dc4726-3cee-4d4c-a820-6ca4047845d8';
+const supabaseAnonKey = import.meta.env.SUPABASE_KEY;
+const hCaptchaSiteKey = import.meta.env.hCAPTCHA_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error(
