6c838e265f20a21ea18ad1d9088e457f1f346c87
pardod <dhunterpardo@gmail.com>
2024-11-14 00:59:02 -0500

    What

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -6,7 +6,7 @@ const hCaptchaSiteKey = import.meta.env.hCAPTCHA_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error(
-    'Please set VITE_SUPABASE_URL and VITE_SUPABASE_ANON_KEY environment variables. You can find these values in your Supabase project settings.'
+    'Please set VITE_SUPABASE_URL and VITE_SUPABASE_ANON_KEY environment variables. You can find these values in your Supabase project settings .'
   );
 }
 
